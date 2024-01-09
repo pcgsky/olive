27bfcddf3987691c82b1414dd2005985e9dcd8f3
pcgsky <lifestream686@gmail.com>
2024-01-09 15:59:10 +0800

    Update douyin.go

--- foundation/olivetv/douyin.go
+++ foundation/olivetv/douyin.go
@@ -88,8 +88,8 @@ func (this *douyin) set(tv *TV) error {
 	if err != nil {
 		return err
 	}
-	flv := streamData.Data.Origin.Main.Flv
-	hls := streamData.Data.Origin.Main.Hls
+	flv := streamData.Data.Sd.Main.Flv
+	hls := streamData.Data.Sd.Main.Hls
 	_ = hls
 	tv.streamURL = flv
 	tv.roomOn = true
